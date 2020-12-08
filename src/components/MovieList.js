48d98100b452a3eee4c6536de577f58e83c528e5
MohamedAli Mahdhaoui <med_ali_mahdhaoui@yahoo.fr>
2020-12-08 22:26:47 +0100

    fixed bugs but still have a bug with rating filter

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -16,28 +16,28 @@ const MovieList = () => {
       title: "Gladiator",
       description:
         "The Roman general Maximus is the most faithful support of the emperor Marcus Aurelius, whom he led from victory to victory. Jealous of Maximus 'prestige, and even more of the emperor's love for him, Marcus Aurelius' son, Commodus, brutally arrogates himself to power, then orders the general's arrest and execution. Maximus escapes his assassins, but cannot prevent the slaughter of his family. Captured by a slave trader, he becomes a gladiator and prepares his revenge.",
-      rate: 5,
+      rate: "5",
       imgSrc: "/Gladiator.jpg",
     },
     {
       title: "Shawshank Redemprion",
       description:
         "In 1947, Andy Dufresne, a young banker, was sentenced to life in prison for the murder of his wife and her lover. Despite claiming his innocence, he was imprisoned in Shawshank, the most severe penitentiary in the state of Maine. There he meets Red, a disillusioned man, detained for 20 years. Then begins a great story of friendship between the two men.",
-      rate: 3,
+      rate: "3",
       imgSrc: "/TheShawshank.jpg",
     },
     {
       title: "Spy Game ",
       description:
         "At the time of leaving the CIA, Nathan Muir learns that his ex-partner, the young tom Bishop, has just been captured in China during a daring operation. Accused of espionage, he will be executed within 24 hours. The CIA, fearing an international incident, refuses to save him. Forgetting the resentment, Nathan decides to take care of it himself. For him then begins his most personal and most dangerous mission.",
-      rate: 2,
+      rate: "2",
       imgSrc: "/SpyGame.jpg",
     },
     {
       title: "Titanic",
       description:
         "In 1997, the wreckage of the Titanic was the subject of a feverish exploration, led by treasure seekers in search of a blue diamond that emerges on board. Struck by a television report, one of the survivors of the shipwreck, aged 102, Rose DeWitt, goes there and talks about her memories. 1912. Engaged to an arrogant industrialist, Rose meets a penniless artist on the boat.",
-      rate: 5,
+      rate: "5",
       imgSrc: "/Titanic.jpg",
     },
   ];
@@ -74,7 +74,7 @@ const MovieList = () => {
   //Mtheod passed as prop in child component (FilterMv) to handle searched movie name value change
   const onSearchHandler = (inputTitle) => {
     setSearchMvbyName(inputTitle);
-    inputTitle === ""
+    inputTitle == ""
       ? setList(movieList)
       : setList(
           List.filter((movie) =>
@@ -88,7 +88,7 @@ const MovieList = () => {
   //Method passed as prop in child componenet (FilterMv) to handle rate value change
   const onSetrateHandler = (rateValue) => {
     setsearchMvByRate(rateValue);
-    rateValue === 0
+    rateValue == 0
       ? setList(movieList)
       : setList(List.filter((movie) => movie.rate > searchMvByRate));
   };
